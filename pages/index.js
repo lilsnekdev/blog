375609bd5226ca4add3dfe4202638cbb1d37c830
Szymon Tarczewski <szymon.tarczewski@gmail.com>
2022-11-09 16:52:58 +0100

    fix: text fix

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,6 @@ export default function Home({ allPostsData }) {
           Hello! I'm a frontend developer at NASK's Agile Software Development
           Department.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
